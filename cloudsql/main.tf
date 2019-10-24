b0ac73dfae7356b11b15f8a677dcc87b6380f9f9
sunm847 <54668641+sunm847@users.noreply.github.com>
2019-10-24 16:29:15 +0100

    Create main.tf

---
+++ cloudsql/main.tf
@@ -0,0 +1,51 @@
+provider "random" {}
+
+resource "random_id" "id" {
+  byte_length = 4
+  prefix      = "sql-${terraform.workspace}-"
+}
+
+resource "google_sql_database_instance" "master" {
+#  name             = "sql-${terraform.workspace}-master"
+  name             = "${random_id.id.hex}"
+  region           = "${var.region}"
+  database_version = "POSTGRES_9_6"
+
+  settings {
+    availability_type = "${var.availability_type[terraform.workspace]}"
+    tier              = "${var.sql_instance_size}"
+    disk_type         = "${var.sql_disk_type}"
+    disk_size         = "${var.sql_disk_size}"
+    disk_autoresize   = true
+
+    ip_configuration {
+      authorized_networks = {
+        value = "0.0.0.0/0"
+      }
+
+      require_ssl  = "${var.sql_require_ssl}"
+      ipv4_enabled = true
+    }
+
+    location_preference {
+      zone = "${var.region}-${var.sql_master_zone}"
+    }
+
+    backup_configuration {
+#      binary_log_enabled = true
+      enabled            = true
+      start_time         = "00:00"
+    }
+  }
+}
+
+
+resource "google_sql_user" "user" {
+  depends_on = [
+    "google_sql_database_instance.master",
+  ]
+
+  instance = "${google_sql_database_instance.master.name}"
+  name     = "${var.sql_user}"
+  password = "${var.sql_pass}"
+}
