e4912b33f90e940973a20ec6217fe767adc639eb
sunm847 <54668641+sunm847@users.noreply.github.com>
2019-10-24 12:01:01 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  default = "sensyne-cluster"
+}
+variable "project" {
+  default = "sensyne-project-123456"
+}
+
+variable "location" {
+  default = "us-central1"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"  
+}
