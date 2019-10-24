c36ad606a6682b23c805c39e5141a13fa84596e2
sunm847 <54668641+sunm847@users.noreply.github.com>
2019-10-24 19:29:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "name" {
   default = "sensyne-cluster"
 }
+
 variable "project" {
   default = "sensyne-project-123456"
 }
@@ -16,3 +17,13 @@ variable "initial_node_count" {
 variable "machine_type" {
   default = "n1-standard-1"  
 }
+
+variable "admin_username"{
+  default = "Sensyne"
+}
+
+variable "admin_password"{
+  default = "sensyne1234"
+}
+
+
